8190da608f4d3fe8213464cb95cde3e0312a6294
AditiRupade <30768250+AditiRupade@users.noreply.github.com>
2022-04-10 00:08:52 +0530

    add class model

---
+++ game.py
@@ -0,0 +1,19 @@
+class game:
+    graph = graph()
+    user = user()
+    current_node = 0
+    max_color = 0
+    time = 0
+    #entanglement_map ??
+
+    def next_move(graph, current_node):
+        #not_implemented
+
+    def check_move():
+        #not_implemented
+
+    def update_entanglement_map():
+        #not_implemented
+        
+    def find_chromatic_number():
+        #not_implemented
---
+++ graph.py
@@ -0,0 +1,5 @@
+class graph:
+    number_of_nodes = 0
+
+    def generate_graph(number_of_nodes):
+        #not_implemented
